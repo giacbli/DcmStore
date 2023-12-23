ba2700f5bf54e1ab458d5212a6acbe6e1d4c5d7d
giacbli <giacbli@libero.it>
2023-12-23 23:39:53 +0100

    Update install-service.bat

--- install-service.bat
+++ install-service.bat
@@ -1,4 +1,4 @@
 @echo off
-sc create "DcmStoreService" binPath= .\release\DcmStoreService.exe start= auto displayname= "Dicom Store Service"
+sc create "DcmStoreService" binPath= .\bin\DcmStoreService.exe start= auto displayname= "Dicom Store Service"
 sc description "DcmStoreService" "Run a simple Dicom Storage server"
 pause
