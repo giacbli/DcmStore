ce2d988ec6c731d42c26559b680b75919628cf6d
giacbli <giacbli@libero.it>
2023-12-29 12:24:59 +0100

    Add files via upload

--- install-service.bat
+++ install-service.bat
@@ -1,4 +1,7 @@
 @echo off
-sc create "DcmStoreService" binPath= .\bin\DcmStoreService.exe start= auto displayname= "Dicom Store Service"
+
+set DCMSCP=%~dp0
+sc create "DcmStoreService" binPath= "%DCMSCP%bin\DcmStoreService.exe" start= auto displayname= "Dicom Store Service"
 sc description "DcmStoreService" "Run a simple Dicom Storage server"
+
 pause
